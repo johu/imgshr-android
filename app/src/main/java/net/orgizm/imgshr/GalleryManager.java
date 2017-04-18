3742545a5ca8095a2279775cd6858fd1a497f2fc
henning mueller <mail@nning.io>
2017-04-18 14:02:24 +0200

    Customize styling slightly

--- app/src/main/java/net/orgizm/imgshr/GalleryManager.java
+++ app/src/main/java/net/orgizm/imgshr/GalleryManager.java
@@ -2,6 +2,7 @@ package net.orgizm.imgshr;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
 
 public class GalleryManager extends Activity {
 
@@ -9,6 +10,9 @@ public class GalleryManager extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery_manager);
+
+        CollapsingToolbarLayout layout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        layout.setTitle(getString(R.string.app_name));
     }
 
 }
